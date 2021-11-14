44653c2b73e04db5f0bf9b03f58c858b9450dd04
Jestin VanScoyoc <coyotepod@gmail.com>
2021-11-14 15:43:50 -0500

    Create Robot.java

---
+++ 2022-Team5235/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+package frc.robot;
+
+// This section is for adding libraries of code. These may need to be added
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
+ * arcade steering.
+ */
+
+ // 
+public class Robot extends TimedRobot {
+ private final Talon first;
+ private final Talon zeroth;
+  @Override
+  public void teleopPeriodic() {
+    // Drive with arcade drive.
+    // That means that the Y axis drives forward
+    // and backward, and the X turns left and right.
+    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+  }
+}
